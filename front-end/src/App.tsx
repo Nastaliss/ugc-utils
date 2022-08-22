bd6f89b55dc824e61db8f5692ef3c2bbdc465fbd
Stanislas Bruhiere <stanbill91@gmail.com>
2022-08-23 01:10:44 +0200

    Add react frontend

---
+++ front-end/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+import {Map} from './map/Map';
+
+function App() {
+  return (
+    <Map/>
+  );
+}
+
+export default App;
---
+++ front-end/src/map/Map.tsx
@@ -0,0 +1,40 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
+
+export const Map = () => {
+
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [theatres, setTheatres] = useState([])
+
+    useEffect(() => {
+        console.log("here")
+        axios.get("http://localhost:5000/theatres").then((r) => {
+            console.log(r);
+            setTheatres(r.data);
+            setIsLoaded(true);
+        }, (e) => {
+            setIsLoaded(false);
+            console.log(e);
+
+        });
+}, [] )
+  return (
+    <MapContainer center={[
+        48.859801, 2.346648]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        { theatres.map((theatre, index) => (
+            <Marker position={[theatre["lat"], theatre["lon"]]} key={index}>
+                <Popup>
+                {theatre["name"]}
+                </Popup>
+            </Marker>
+        ))}
+
+      </MapContainer>
+  )
+}
